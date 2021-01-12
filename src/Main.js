c1a093ee42f5e9c5df308488ae3a822cab6fcb62
toniemati <matikas2000@gmail.com>
2021-01-12 12:24:17 +0100

    more command and auto remove

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv';
-import { Client } from 'discord.js';
+import { Client, MessageFlags } from 'discord.js';
 import Joke from './Joke.js';
 import Bot from './Bot.js';
 import Biba from './Biba.js';
+
+import { beers } from '../beers.js';
 
 dotenv.config();
 
@@ -24,15 +26,61 @@ class Main {
     });
 
     CLIENT.on('message', (message) => {
+      //* Check channel
+      if (!this.checkChannel(message)) return;
+
+      //* If bot return
       if (message.author.bot) return;
 
+      //* check piwo
+      this.checkPiwo(message);
+
+      //* Chech if message contains 'robert'
       this.checkRobertWord(message);
 
+      //* Check jokes command
       JOKE.checkJoke(message);
+
+      //* Check bot command
       BOT.checkBot(message);
+
+      //* Chech biba command
       BIBA.checkBiba(message);
+
+      //* Auto delete message
+      this.autoDelete(message);
     });
 
+  }
+
+  checkChannel(message) {
+    const { channel: { id } } = message;
+
+    //* 1 - test
+    //* 2- piwo
+    if (id !== '798496100856102922' && id !== '798498303859752960') return false;
+    else return true;
+  }
+
+  checkPiwo(message) {
+    const { content } = message;
+    if (content === '!piwo') {
+      message.react('🍺');
+
+      const index = Math.floor(Math.random() * (beers.length - 0)) + 0;
+      const beer = beers[index];
+
+      message.channel.send(beer);
+    }
+  }
+
+  autoDelete(message) {
+    setTimeout(() => {
+      message.channel.messages.fetch()
+        .then((results) => {
+          message.channel.bulkDelete(results);
+        })
+    }, 10000);
   }
 
   checkRobertWord(message) {
